49e39bbcb90d82142c5446d97b27943c583a7337
kevintx28 <kevin.tolachoque@gmail.com>
2022-05-13 16:13:21 -0500

    creando proyecto

---
+++ src/main/java/com/examen1/App.java
@@ -0,0 +1,13 @@
+package com.examen1;
+public class App 
+
+{
+    public static void main( String[] args )
+    {
+        // crear una clases products, hacer el crud ; crear, lectura, actualizar, delete
+        
+        products clase_tomate = new products();
+
+    }
+
+}
